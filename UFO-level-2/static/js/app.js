13b12c0ecee511d1b17785f4c64bd1fd79f687b6
capflaum <69134400+capflaum@users.noreply.github.com>
2020-11-23 19:24:54 -0800

    committed 11/23

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,44 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach((sightingEvent) => {
+    console.log(sightingEvent);
+    var row = tbody.append("tr");
+    Object.entries(sightingEvent).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Date Search
+
+var filter = d3.select("#filters");
+var button = d3.select("#filter-btn");
+
+filter.on("submit", runEnter);
+button.on("click", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#input");
+    var inputValue = inputElement.property("value");
+
+    //console.log(inputValue);f
+
+    var filteredData = tableData.filter(data => data.datetime === inputValue || data => data.city === inputValue || data => data.state === inputValue || data => data.country === inputValue || data => data.shape === inputValue);
+    console.log(filteredData);
+
+    // Display filtered results
+    filteredData.forEach((searchFilter) => {
+        console.log(searchFilter);
+        var row = tbody.append("tr");
+        Object.entries(searchFilter).forEach(([key, value]) => {
+            console.log(key,value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
