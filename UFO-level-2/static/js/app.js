a2b2401fc7dbb543a1de20bf67db368c6c7ee418
capflaum <69134400+capflaum@users.noreply.github.com>
2021-09-30 13:40:39 -0700

    update level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,51 +1,92 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
 var tbody = d3.select("tbody");
 
-tableData.forEach((sightingEvent) => {
-    console.log(sightingEvent);
-    var row = tbody.append("tr");
-    Object.entries(sightingEvent).forEach(([key, value]) => {
-        console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
-
-// Date Search
-
-var filter = d3.select("#filters");
-var button = d3.select("#filter-btn");
-
-filter.on("submit", runEnter);
-button.on("click", runEnter);
-
-function runEnter() {
-    d3.event.preventDefault();
-    var inputElement = d3.select("#input");
-    var inputValue = inputElement.property("value");
-
-    //console.log(inputValue);f
-
-    var filteredData = tableData.filter(data => data.datetime === inputValue || 
-        data.city === inputValue || 
-        data.state === inputValue || 
-        data.country === inputValue || 
-        data.shape === inputValue);
-    console.log(filteredData);
-
-    // Display filtered results
-    filteredData.forEach((searchFilter) => {
-        console.log(searchFilter);
+function buildTable(data) {
+    tbody.html("");
+    data.forEach((dataRow) => {  
+    // console.log(dataRow);
         var row = tbody.append("tr");
-        Object.entries(searchFilter).forEach(([key, value]) => {
-            console.log(key,value);
-            var cell = row.append("td");
-            cell.text(value);
-
-    list.append(tbody).text(filteredData);
+        Object.values(dataRow).forEach((val) => {
+            // console.log(val);
+        var cell = row.append("td");
+        cell.text(val);
         });
     });
-};
+}
+
+// Date Search
+var filters = {};
+console.log(filters);
+function updateFilters() {
+    var changedElement = d3.select(this).select("input");
+    var elementValue = changedElement.property("value");
+    var filterID = changedElement.attr("id");
+
+
+    if(elementValue) {
+        filters[filterID] = elementValue;
+    }
+    else {
+        delete filters[filterID];
+    }
+
+filterTable();
+}
+
+function filterTable() {
+    let filteredData = tableData;
+
+    Object.entries(filters).forEach(([key, value]) => {
+        filteredData = filteredData.filter(row => row[key] === value);
+    });
+
+    buildTable(filteredData);
+}
+
+d3.selectAll(".filter").on("change", updateFilters);
+
+
+buildTable(tableData);
+
+
+
+
+// previous attempt:
+
+// var filter = d3.select("#filters");
+// var button = d3.select("#filter-btn");
+
+// filter.on("submit", runEnter);
+// button.on("click", runEnter);
+
+// function runEnter() {
+//     d3.event.preventDefault();
+//     var inputElement = d3.select("#input");
+//     var inputValue = inputElement.property("value");
+
+    //console.log(inputValue);
+
+//     var filteredData = tableData.filter(data => data.datetime === inputValue || 
+//         data.city === inputValue || 
+//         data.state === inputValue || 
+//         data.country === inputValue || 
+//         data.shape === inputValue);
+//     console.log(filteredData);
+
+//     // Display filtered results
+//     filteredData.forEach((searchFilter) => {
+//         console.log(searchFilter);
+//         var row = tbody.append("tr");
+//         Object.entries(searchFilter).forEach(([key, value]) => {
+//             console.log(key,value);
+//             var cell = row.append("td");
+//             cell.text(value);
+
+//     list.append(tbody).text(filteredData);
+//         });
+//     });
+// };
+
